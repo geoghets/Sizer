a377367be1434f1c3eb94ff689ee4f68dfd4a897
Thomas Geoghegan <tbgeoghegan@gmail.com>
2025-11-02 21:23:33 -0500

    first commit!

---
+++ main.cmd
@@ -0,0 +1,7 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'main.exe' with Python DLL being found.
+set PATH=c:\users\tbgeo\appdata\local\programs\python\python311;%PATH%
+set PYTHONHOME=C:\Users\tbgeo\AppData\Local\Programs\Python\Python311
+set NUITKA_PYTHONPATH=C:\Users\tbgeo\source\repos\Sizer;C:\Users\tbgeo\AppData\Local\Programs\Python\Python311\DLLs;C:\Users\tbgeo\AppData\Local\Programs\Python\Python311\Lib;C:\Users\tbgeo\AppData\Local\Programs\Python\Python311;C:\Users\tbgeo\AppData\Local\Programs\Python\Python311\Lib\site-packages;C:\Users\tbgeo\AppData\Local\Programs\Python\Python311\Lib\site-packages\win32;C:\Users\tbgeo\AppData\Local\Programs\Python\Python311\Lib\site-packages\win32\lib;C:\Users\tbgeo\AppData\Local\Programs\Python\Python311\Lib\site-packages\Pythonwin
+"%~dp0main.exe" %*
---
+++ main.py
@@ -0,0 +1,29 @@
+import time
+# import numpy
+import pandas as pd
+import pynput
+
+data = {
+    "a":[1,2,3,4,5],
+    "b":[5,3,6,2,2],
+    "c":[9,9,2,2,2]
+}
+df = pd.DataFrame(data)
+
+print(df)
+time.sleep(2)
+
+def pirnt():
+    print("Hello world")
+def main():
+
+    with pynput.keyboard.GlobalHotKeys({
+        '<f11>': pirnt()
+
+    }) as h:
+        h.join()      
+
+if __name__ == "main":
+    main()
+
+
